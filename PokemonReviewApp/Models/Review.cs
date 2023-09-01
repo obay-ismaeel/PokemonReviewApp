c69f89ad5e0929031297c27ba87dc3539f10a25b
Obay Ismaeel <obayhany@gmail.com>
2023-09-01 13:29:55 -0400

    Initial commit with main models created

---
+++ PokemonReviewApp/Models/Review.cs
@@ -0,0 +1,16 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public Pokemon Pokemon { get; set; }
+
+        public Reviewer Reviewer { get; set; }
+
+    }
+}
